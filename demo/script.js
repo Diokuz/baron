ce5b8324d903308594e8405b8daa4b7e8ee4b997
Kuznetsov Dmitry <dm.kuznecov@2gis.ru>
2013-03-01 16:14:18 +0700

    Simple demo added

--- demo/script.js
+++ demo/script.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    baron($('.wrapper'), {
+    baron($('.wrapper_1'), {
         scroller: '.scroller',
         container: '.container',
         bar: '.scroller__bar',
@@ -9,4 +9,6 @@ window.onload = function() {
         hTopFixCls: 'header__title_position_top',
         hBottomFixCls: 'header__title_position_bottom'
     });
+
+    $('.wrapper_2').baron({barOnCls: 'scroller__bar_state_on'});
 };
