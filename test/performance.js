2bd61e73ea54068a6278529cc3e967e1c21ae748
Kuznetsov Dmitry <dm.kuznecov@2gis.ru>
2013-12-04 14:45:21 +0700

    perf test upd

--- test/performance.js
+++ test/performance.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
     var N = 1000;
 
-    function test(callback) {
+    function test(callback, n) {
         var t0 = new Date();
         var scrollHeight = $('.search-results__scroller')[0].scrollHeight - $('.search-results__scroller').height();
         var scroller = $('.search-results__scroller')[0];
+        var localN = n || N;
 
         i = 0;
         var interval = setInterval(function() {
@@ -13,34 +14,39 @@ $(document).ready(function() {
 
             scroller.scrollTop = pos;
 
-            if (i > N) {
+            if (i > localN) {
                 clearTimeout(interval);
                 var t1 = new Date();
-                console.log('r&r per scroll: ', (t1 - t0) / N);
+                if (!n) console.log('r&r per scroll: ', (t1 - t0) / N);
                 callback(t1 - t0);
             }
         }, 0);
     }
 
     setTimeout(function() {
-        test(function(result1) {
-            $('.search-results__scroller').baron({
-                bar: '.search-results__scroll-bar',
-                barOnCls: 'search-results_scroll-bar_active'
-            });
-            // .fix({
-            //     elements: '.header__title',
-            //     outside: 'header__title_state_fixed',
-            //     before: 'header__title_position_top',
-            //     after: 'header__title_position_bottom',
-            //     limiter: true
-            // });
+        test(function() { // Первый прогон для оптимизации
+            test(function(result1) {
+                $('.search-results__scroller').baron({
+                    bar: '.search-results__scroll-bar',
+                    barOnCls: 'search-results_scroll-bar_active'
+                })
+                // .fix({
+                //     elements: '.search-results__fade',
+                //     outside: 'search-results__fade_state_fixed',
+                //     before: 'search-results__fade_pos_top',
+                //     after: 'search-results__fade_pos_bottom',
+                //     past: 'search-results__fade_group_top',
+                //     future: 'search-results__fade_group_bottom',
+                //     clickable: true,
+                //     limiter: true // Ограничение скроллбара сверху
+                // });
 
-            setTimeout(function() {
-                test(function(result2) {
-                    console.log('delta per scroll: ', (result2 - result1) / N);
-                });
-            }, 5000);
-        });
-    }, 5000);
+                setTimeout(function() {
+                    test(function(result2) {
+                        console.log('delta per scroll: ', (result2 - result1) / N);
+                    });
+                }, 500);
+            });
+        }, 300);
+    }, 500);
 });
